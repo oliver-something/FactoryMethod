9cc79397d9283296868da3909e33a98f106376ab
Khashayar Hosseini <diablo31@live.co.uk>
2024-06-28 23:07:37 +0330

    Finished Project

--- main.cpp
+++ main.cpp
@@ -1,12 +1,64 @@
 #include <iostream>
-enum class Products {
+#include <memory>
+#include <string_view>
+
+enum class ProductId {
     FIRST_PRODUCT,
     SECOND_PRODUCT
 };
 
+class Product {
+public:
+    virtual void Print() = 0;
+    virtual std::string_view ToString() = 0;
+    virtual ~Product() = default;
+};
 
+class FirstConcreteProduct : public Product {
+public:
+    void Print() override {
+        std::cout << "This is from " << this->ToString() << '\n';
+    }
+
+    std::string_view ToString() override {
+        return "First Product";
+    }
+};
+
+class SecondConcreteProduct : public Product {
+public:
+    void Print() override {
+        std::cout << "This is from " << this->ToString() << '\n';
+    }
+
+    std::string_view ToString() override {
+        return "Second Product";
+    }
+};
+
+class Creator {
+public:
+    [[nodiscard]] virtual std::unique_ptr<Product> create(ProductId id) const {
+        switch (id) {
+
+            case ProductId::FIRST_PRODUCT:
+                return std::make_unique<FirstConcreteProduct>();
+            case ProductId::SECOND_PRODUCT:
+                return std::make_unique<SecondConcreteProduct>();
+            default:
+                return nullptr;
+        }
+    }
+    virtual ~Creator() = default;
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::unique_ptr<Creator> creator = std::make_unique<Creator>();
+    std::unique_ptr<Product> product;
+
+    product = creator->create(ProductId::FIRST_PRODUCT);
+    product->Print();
+    product = creator->create(ProductId::SECOND_PRODUCT);
+    product->Print();
     return 0;
 }
